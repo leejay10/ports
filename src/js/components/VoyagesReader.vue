6f4953ec5555881a7c1a8bec33c1d5b7ec81c213
Jamie <jamienevillelee@gmail.com>
2020-02-02 06:34:51 +0000

    up

--- src/js/components/VoyagesReader.vue
+++ src/js/components/VoyagesReader.vue
@@ -1,17 +1,19 @@
 <template>
-	<div class="w-1/4 px-2">
-		<p>Morale</p>
-		<input type="text" name="morale" id="morale" v-model="morale" class="input p-2 text-center" @click="selectText">
-	</div>
+	<div>
+		<div class="w-1/4 px-2">
+			<p>Morale</p>
+			<input type="text" name="morale" id="morale" v-model="morale" class="input p-2 text-center" @click="selectText">
+		</div>
 
-	<div class="w-1/4 px-2">
-		<p>Combat</p>
-		<input type="text" name="combat" id="combat" v-model="combat" class="input p-2 text-center" @click="selectText">
-	</div>
+		<div class="w-1/4 px-2">
+			<p>Combat</p>
+			<input type="text" name="combat" id="combat" v-model="combat" class="input p-2 text-center" @click="selectText">
+		</div>
 
-	<div class="w-1/4 px-2">
-		<p>Seafaring</p>
-		<input type="text" name="seafaring1" id="seafaring" v-model="seafaring" class="input p-2 text-center" @click="selectText">
+		<div class="w-1/4 px-2">
+			<p>Seafaring</p>
+			<input type="text" name="seafaring1" id="seafaring" v-model="seafaring" class="input p-2 text-center" @click="selectText">
+		</div>
 	</div>
 </template>
 
@@ -35,10 +37,10 @@
 			read(){
 				let success = this.reader.read();
 
+
 				if(success){
 					let result = this.reader.result;
 
-					console.log('VoyageReaderResult', result);
 
 					
 				}
